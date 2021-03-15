ad09cffaa917e8e7e9f97b34e1b7d65280dc5467
naymajahan <naymajahanshoshe@gmail.com>
2021-03-15 16:08:45 +0600

    conditional added

---
+++ conditional.py
@@ -0,0 +1,25 @@
+# Conditional Statement
+
+
+if 3 < 2 :
+    print("3 is greater than 2")
+elif 5 > 10 :
+    print("5 is less than 10")
+else :
+    print("all statements are wrong")
+print("python is cool")
+
+
+
+
+## Robot Moving
+
+print('Enter Your Command')
+robot = input()
+
+if robot == 'front':
+    print("Moving Front")
+elif robot == 'back':
+    print("Moving Back")
+else :
+    print("Stand Still")
