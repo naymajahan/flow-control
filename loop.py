457d8cf4187d8e0a32f4bf4519da16c7fa5c9b33
naymajahan <naymajahanshoshe@gmail.com>
2021-03-15 19:54:44 +0600

    While & For Loop

---
+++ loop.py
@@ -0,0 +1,56 @@
+#Loops
+## 2 types in python
+### While loop;;;; While loop;;; while loop;;; while loop
+## It will print 1-6
+
+x = 0
+while x<7:
+    print(x)
+    x = x+1
+print("it's a while loop")
+
+
+#### it will print 1-7
+
+x = 0
+while x<=7:
+    print(x)
+    x = x+1
+print("it's a while loop")
+
+
+### FOR LOO;;;; FOR LOOP ;;; FOR LOOP;;; FOR LOOP
+# 3 parameters- range (start, end , stepsize)
+
+for i in range(1,4):
+    print("Hello")
+print('I am for loop')
+
+for i in range(0,10,2):
+    print("hello")
+
+
+## For loop with int 
+
+
+for i in range(1,4):
+    print(i)
+print('I am for loop')
+
+for i in range(0,10,2):
+    print(i)
+
+
+### For Loop with negative point
+
+for i in range(-1,-14 , -3):
+    print(i)
+
+print('I am for loop')
+
+for i in range(0,-8,-2):
+    print(i)
+
+
+
+
